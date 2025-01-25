8956296fde24e474e78dd353db778c057c6da679
NARESH-ASHOK-MALI <nareshashokmali@gmail.com>
2025-01-25 22:02:32 +0530

    Files to commit:     DSAL/DSAL4.exe     DSAL/test.cpp     DSAL/test.exe     HTML/Firsttest/index.html     HTML/Firsttest/style.css     HTML/Hackseries/desktop.ini -> HTML/Portfolio/desktop.ini     HTML/Hackseries/index.html -> HTML/Portfolio/index.html     HTML/Hackseries/naresh.png -> HTML/Portfolio/naresh.png     HTML/Hackseries/script.js -> HTML/Portfolio/script.js     HTML/Hackseries/style.css -> HTML/Portfolio/style.css

--- DSAL/test.cpp
+++ DSAL/test.cpp
@@ -4,13 +4,16 @@
 using namespace std;
 int main(){
     int n;
+    int avgspeed;
     string city[20];
-    int adj_matrix[50][50];
+    int time_matrix[50][50];
+    int km_matrix[50][50];
     cout << "Enter the no. of cities : ";
     cin >> n;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            adj_matrix[i][j] = 0;
+            time_matrix[i][j] = 0;
+            km_matrix[i][j]=0;
         }
     }
 
@@ -18,15 +21,20 @@ int main(){
         cout << "Enter the name of city " << i+1 << ":";
         cin >> city[i];
     }
+    cout<<"Enter the average speed of vehicle you may travel with:"<<endl;
+    cin>>avgspeed;
     for (int i = 0; i < n; i++){
     for (int j = i+1; j < n; j++){
-        cout << "Enter the time required to travel from city " << city[i] << " to the " << city[j] << " " << endl;
-        cin >> adj_matrix[i][j];
-        adj_matrix[j][i] = adj_matrix[i][j];
+        cout << "Enter the distance from city " << city[i] << " to the " << city[j] << " " << endl;
+        cin >> km_matrix[i][j];
+        km_matrix[j][i] = km_matrix[i][j];
+        time_matrix[i][j]= km_matrix[i][j]/avgspeed;
+        time_matrix[j][i] = time_matrix[i][j];
+
         }
     }
-    cout << "\nAdjacency Matrix:\n\n";
-    cout << "   ";
+    cout << "\nAdjacency Matrix: of Distances\n\n";
+    cout << "\t";
     for (int i = 0; i < n; i++) {
         cout << city[i] << "\t";
     }
@@ -34,7 +42,20 @@ int main(){
     for (int i = 0; i < n; i++) {
         cout << city[i] << "\t";
         for (int j = 0; j < n; j++) {
-            cout << adj_matrix[i][j] << "\t";
+            cout << km_matrix[i][j] << "\t";
+        }
+    cout << "\n";
+    }
+     cout << "\nAdjacency Matrix: of time required :\n\n";
+    cout << "\t";
+    for (int i = 0; i < n; i++) {
+        cout << city[i] << "\t";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << city[i] << "\t";
+        for (int j = 0; j < n; j++) {
+            cout << time_matrix[i][j] << "\t";
         }
     cout << "\n";
     }
