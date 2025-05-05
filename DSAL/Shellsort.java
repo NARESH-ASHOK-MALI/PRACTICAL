ad5d4fafb8c163ce3ce3865365cae5d16dd1dbd5
NARESH-ASHOK-MALI <nareshashokmali@gmail.com>
2025-05-05 21:34:15 +0530

    extras

--- DSAL/Shellsort.java
+++ DSAL/Shellsort.java
@@ -1,27 +1,42 @@
+import java.util.Scanner;
 class shellsort{
+    
     static void printArray(int arr[]){
         int n=arr.length;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             System.out.print(arr[i]  + " ");
+        }
         System.out.println();
     }
 
-
     static int sort(int arr[]){
         int n=arr.length;
-        for (int gap = n/2; gap > 0; gap /= 2)
-        {for (int i = gap; i < n; i += 1)
-            {   int temp = arr[i];int j;
-                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+        for (int gap = n/2; gap > 0; gap=( gap /2)){
+            for (int i = gap; i < n; i++){
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j=j- gap){
                     arr[j] = arr[j - gap];
+                }
                 arr[j] = temp;
             }
         }
         return 0;
     }
-    
+
     public static void main(String[] args) {
-        int[] arr = { 5, 3, 4, 2, 1 };
+        //make useer input to create an array
+        
+  
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. elements of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++){
+           arr[i] = sc.nextInt();
+        }
+        sc.close();
         System.out.print("Orignal array: ");
         printArray(arr);
         System.out.print("sorted array: ");
