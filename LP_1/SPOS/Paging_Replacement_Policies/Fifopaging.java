eb2bb25f0ad88dd0e15f3041bb8e866ce84718b9
NARESH ASHOK MALI <nareshashokmali@gmail.com>
2025-09-23 12:46:18 +0530

    I am uploading material

--- LP_1/SPOS/Paging_Replacement_Policies/Fifopaging.java
+++ LP_1/SPOS/Paging_Replacement_Policies/Fifopaging.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class Fifipaging {
+public class Fifopaging {
     public static void main(String[] args) throws IOException 
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
